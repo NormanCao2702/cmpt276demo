a9f3ded007fe5de3b4d4b427c08a64d424e661c2
NormanCao2702 <norman.cao2702@gmail.com>
2023-06-09 11:42:46 -0700

    Updated UsersController and deleted Demo2Application and Users models

--- src/main/java/demo/controllers/UsersController.java
+++ src/main/java/demo/controllers/UsersController.java
@@ -1,41 +1,43 @@
 package demo.controllers;
-
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import models.Users;
+import models.User;
+import models.UserRepository;
 
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
-public class UsersController {
+public class UsersController {  
     
-    // @Autowired
-    // private UserRepository userRepo;
+    @Autowired
+    private UserRepository userRepo;
 
     @GetMapping("/users/view")
     public String getAllUsers(Model model){
         System.out.println("Getting all users");
-        // TODO: get all users from database
-        List<Users> users = new ArrayList<>();
-        users.add(new Users("bobby","1234",25));
-        users.add(new Users("sara","1234",15));
-        users.add(new Users("steve","1234",35));
-        users.add(new Users("jacob","1234",5));
+        // get all users from database
+        List<User> users = userRepo.findAll();
         // end of database call
         model.addAttribute("us", users);
-        return "users/showAll.html";
+        return "/users/showAll";
     }
 
-    // @GetMapping(value="/users/add")
-    // public String AddUsers(){
-    //     Users newuser = new Users("henry", "1234",25);
-    //     userRepo.save(newuser);
-    //     return "users/added";
-    // }
-
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response){
+        System.out.println("ADD user");
+        String newName = newuser.get("name");
+        String newPwd = newuser.get("password");
+        int newSize = Integer.parseInt(newuser.get("size"));
+        userRepo.save(new User(newName,newPwd,newSize));
+        response.setStatus(201);
+        return "/users/addedUser";
+    }
 }
